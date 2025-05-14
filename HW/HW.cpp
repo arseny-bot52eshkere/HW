2b8220fc6653c756e9eab90bf911382f7abc0a34
Николай Горин <gorinno05@mail.ru>
2025-05-14 14:17:38 +0300

    Создана ещё одна функция

--- HW/HW.cpp
+++ HW/HW.cpp
@@ -2,10 +2,15 @@
 
 void newfunction() {
     std::cout << "new function";
+
+}
+void new_funct() {
+    std::cout << "создана ещё одна функция";
 }
 int main()
 {
     std::cout << "woof\n";
     std::cout << "meow " << "Hello World!\n";
+    new_funct();
 }
 
